2220021fabefa43618d130350baacbb592c3f6aa
EngineerPradeep <104459028+EngineerPradeep@users.noreply.github.com>
2024-06-29 21:31:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,263 @@
+// alert("Pappu");
+// alert("Hello World");
+
+// document.getElementById("h").innerHTML = "Raja";
+
+// document.write("kalu");
+// console.log("Hello World");
+
+// var a;
+// a = 2; 
+// This will popup 2  single line comment
+ /* multiple line comment */
+// alert(2);
+// console.log(a);
+
+// new program var
+
+// var digit1 = 21;
+// var x = 10;
+// var z = digit1 + x;
+// console.log(z);
+
+// new let aur const use kare ge
+
+// let y = 'hi';
+// let digit = 10;
+// const z = 2;
+
+// digit = 20;
+// console.log(z);
+
+//  let y = 20;
+//  y = 10;
+//  console.log(y);
+ 
+//  const d = 10;
+//  console.log(d);
+
+
+// Js operator
+ // = mtlb assign
+  
+//    let x;
+//    x = 10;
+//    let y = 3;
+//    x == y;  // equal to operator
+   
+//    console.log(x);
+
+//  let x;
+//  x = 10;
+//  let y = 3;
+ 
+//  let z;
+  
+//   z = x + y;  // addation operator
+  
+//   console.log(z);
+
+// let x;
+// x = 10;
+// let y = 3;
+// let z ;
+  
+//   // power kaise karte h ye dekhe ge
+//     z = x **2;
+ 
+//   z = x + y;
+
+//   x = x + y;
+//   // or same as
+//   x += y;
+//   console.log(z);
+
+
+// let x = 5; // number
+// let y = '5'; // string 
+
+// console.log(x+y);
+
+// let a = "hello";
+// let b = " World";
+// console.log(a+ " " +b);
+
+
+// let x = 5;
+
+// x = x+1;
+// x++;
+// console.log(x);
+ 
+//  let  x = 5;
+ 
+// //  let z = x + 5 / 5;
+// let z = (x + 5) / 5;
+ 
+//  console.log(z);
+
+  /* Data Types in JavaScript  */
+  
+//   let x = 'hel"lo';
+//   console.log(x);
+  
+  
+//   let y = "hel'lo";
+//   console.log(y);
+  
+//   let x = -34.67;
+  
+  // boolen : true or false
+  
+  
+//   let  x = 2>3;
+//    x = 5>2;
+//   console.log(x);
+
+// array 
+
+// let x = ["ram", "tom", "jho"];
+
+// console.log(x);
+
+// object   is  data type
+
+// let man = {name: "raj", age: 50};
+// console.log(man);
+
+// undefined koi value na use ko undefine usko bhi data type hota h
+
+// let x = "";
+
+// console.log(x);
+
+// type oerator 
+
+// let x = typeof "Raj";
+// console.log(x);
+// or 
+// let x = "raj";
+// console.log(typeof x);
+
+// let x = " Raj";
+// // alert("Raj" + x);
+// alert(`hello ${x}`);
+  
+  
+  /* Comparison and Logical Operator */
+  
+//    == equal to
+//    5 ==5 true
+//    6 == 5 false
+//    5 == '5' true
+
+// === equal value and equal type
+
+// 5 === 5 true
+// 6 === 5 false
+// 5 === '5' false
+
+// ! = not equal 
+// 8! = 5 true
+// 5! = "5" true
+// 5! == 8 true
+
+// ! == not equal value or not equal type
+
+//  5 ! == 5 false
+//  5 ! == "5" true
+//  5 ! == 8 true 
+ 
+//  > greater than
+//  5>8 false
+
+// && and
+// (5 < 10 && 6>1) true
+// (5>10 && 6>) false
+ 
+//  || or
+// (5 == 5 || 6 == 5) true
+// (7 == 5 || 6 == 5) false
+
+// ! not 
+// !(6 == 5) true
+
+
+//  If else   
+
+// let age =  15;
+
+// let hasVoterCard = 'No';
+
+// if(age>=14  && hasVoterCard == 'Yes') {
+//   alert("You can vote!");
+// }  else if (age>=14 && hasVoterCard != 'Yes') {
+//   alert(" Get Your voter id card !");
+// }
+
+// else {
+//  alert('You can not vote!');
+// }
+
+
+//    let age = 12;
+   
+//    let hasVoterCard = 'No';
+   
+//    if(age>=14){
+     
+//      if(hasVoterCard == 'Yes'){
+//       alert("You can vote!");
+      
+//      }else{
+//      alert("Get Your voter id card!");
+//      }
+     
+   
+//    }else {
+//      alert('You cannot vote!');
+//    }
+
+// ternary operator 
+
+//   let isLoggedin = 0;  // if 0 logeedout if 1 login
+  
+//   /*
+
+//    if(isLoggedin == 0) {
+//      document.write('Login');
+//    }else{
+//      document.write('Logout');
+//    }
+   
+//    */
+   
+   
+//    let option = isLoggedin  == 1 ? "Logout" : "Login";
+//             //  condition                 true       false  
+//             document.write(option);
+
+ // nalescollision 
+//  let  user ;
+ 
+// //  user = "raja";
+ 
+//  alert(user ?? "Guest User");
+
+
+/*  Switch statement */
+
+//   if input = 1, "y", "yes" output = continue...
+//  if input = 0, "n", output = End...
+
+
+let input;
+ input = "";
+ 
+ if(){
+ 
+ }else{
+ 
+ }
+  
+
